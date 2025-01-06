a73fae095e2a2a0f5108606ce78cacaf13597450
Shark Zubat <akula.puppet.zaba123@gmail.com>
2025-01-06 22:02:53 +0200

    Update help.py

--- bin/help.py
+++ bin/help.py
@@ -3,7 +3,7 @@ import sys
 
 def help():
     bin_path = os.path.dirname(os.path.realpath(__file__))
-    commands = [f[:-3] for f in os.listdir(bin_path) if f.endswith('.py')]
+    commands = [f[:-3] for f in os.listdir(bin_path) if f.endswith('.py') and f != 'registry.py']
     print("Available commands:")
     for command in commands:
         print(command)
