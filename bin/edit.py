8c6b109250dc6eba55e93403154a017a14d7430b
Shark Zubat <akula.puppet.zaba123@gmail.com>
2025-01-09 13:03:07 +0200

    Update edit.py

--- bin/edit.py
+++ bin/edit.py
@@ -1,7 +1,18 @@
-#import
+import sys
 
-#def edit(args):
+def edit(args):
+    if len(args) < 2:
+        print("Usage: edit <filename> <text>")
+        return
 
-#def main(args):
-#    edit(args)
-#    ...
+    file_path = args[0]
+    text = " ".join(args[1:])
+
+    with open(file_path, 'a') as file:
+        file.write(text + "\n")
+
+def main(args):
+    edit(args)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
