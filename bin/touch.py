56780b91c6dcc1448d8cea7c5e344f16e56e6602
Shark Zubat <akula.puppet.zaba123@gmail.com>
2025-01-09 12:57:49 +0200

    Update touch.py

--- bin/touch.py
+++ bin/touch.py
@@ -1,9 +1,19 @@
-# some imports here
+import os
 
-# def touch():
-#     some functions here
+def touch(args):
+    for file_path in args:
+        with open(file_path, 'a'):
+            os.utime(file_path, None)
 
-# def main():
-#     some functions here
+def main(args):
+    if not args:
+        print("Usage: touch <filename>")
+        return
+    
+    touch(args)
+    for file_path in args:
+        print(f"Updated timestamp of {file_path}")
 
-# more
+if __name__ == "__main__":
+    import sys
+    main(sys.argv[1:])
